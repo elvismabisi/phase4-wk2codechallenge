01f7d549c19ed548e6aa428aa0702ff3cf6e8420
Gregory <elvismabisi@gmail.com>
2023-03-27 12:54:25 +0300

    did new migrations

--- db/migrate/20230327095035_create_hero_powers.rb
+++ db/migrate/20230327095035_create_hero_powers.rb
@@ -1,6 +1,8 @@
 class CreateHeroPowers < ActiveRecord::Migration[7.0]
   def change
     create_table :hero_powers do |t|
+      t.string :name
+      t.string :description
 
       t.timestamps
     end
--- db/migrate/20230327095058_create_powers.rb
+++ db/migrate/20230327095058_create_powers.rb
@@ -1,6 +1,9 @@
 class CreatePowers < ActiveRecord::Migration[7.0]
   def change
     create_table :powers do |t|
+      t.string :strength
+      t.integer :power_id
+      t.integer :hero_id
 
       t.timestamps
     end
