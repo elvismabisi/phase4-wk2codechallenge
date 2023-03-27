239eed6953a754ece46248511809e1eb1a22374c
Gregory <elvismabisi@gmail.com>
2023-03-27 12:52:08 +0300

    made new changes to models

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,4 @@
 class Hero < ApplicationRecord
-     # Postman or thunderclient use heros 
-     has_many :hero_powers
-     has_many :powers, through: :hero_powers
+    has_many :hero_powers
+    has_many :powers, through: :hero_powers
 end
